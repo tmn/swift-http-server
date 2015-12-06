b6ceb00588cb09fea5926a786feb3e4b644e01bd
tmn <mail@trimn.net>
2015-12-06 10:56:05 +0100

    Add htons function

--- SwiftServer/Server.swift
+++ SwiftServer/Server.swift
@@ -11,7 +11,7 @@ class Server {
         var serverAddress: sockaddr_in = sockaddr_in(
             sin_len: __uint8_t(sizeof(sockaddr_in)),
             sin_family: sa_family_t(AF_INET),
-            sin_port: _OSSwapInt16(port),
+            sin_port: htons(port),
             sin_addr: in_addr(s_addr: inet_addr("0.0.0.0")),
             sin_zero: (0, 0, 0, 0, 0, 0, 0, 0)
         )
@@ -30,14 +30,26 @@ class Server {
         start()
     }
     
+    
+    
+    /*
+    ------------------------------------------------------------------------------------*/
     func getSockaddrPointer(pointer: UnsafeMutablePointer<Void>) -> UnsafeMutablePointer<sockaddr> {
         return UnsafeMutablePointer<sockaddr>(pointer)
+    }
+    
+    func htons(port: in_port_t) -> in_port_t {
+        return Int(OSHostByteOrder()) == OSLittleEndian ? _OSSwapInt16(port) : port
     }
     
     func sendMessage(socket: Int32, message: String) {
         send(socket,[UInt8](message.utf8), Int(strlen(message)), 0)
     }
     
+    
+    
+    /*
+    ------------------------------------------------------------------------------------*/
     func start() {
         while (true) {
             let client = accept(serverSocket, nil, nil)
