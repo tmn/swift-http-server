77f6e55f84958d52b07b889c1b6ab9b27ca705fe
tmn <mail@trimn.net>
2015-12-06 13:43:01 +0100

    Remove creation info from main.swift

--- SwiftServer/main.swift
+++ SwiftServer/main.swift
@@ -1,11 +1,3 @@
-//
-//  main.swift
-//  SwiftServer
-//
-//  Created by Tri M. Nguyen on 05/12/15.
-//  Copyright © 2015 Tri Nguyen. All rights reserved.
-//
-
 import Foundation
 
 var srv = Server()
