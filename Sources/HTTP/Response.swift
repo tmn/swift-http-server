1d66fddff01eb7f1f6cd44eaa4efb64989dbee73
tmn <mail@trimn.net>
2015-12-08 22:57:14 +0100

    Add sendHeader in Response

--- Sources/HTTP/Response.swift
+++ Sources/HTTP/Response.swift
@@ -5,4 +5,12 @@ public class Response {
     func sendMessage(socket: Int32, message: String) {
         send(socket,[UInt8](message.utf8), Int(strlen(message)), 0)
     }
+    
+    public func sendHeader(socket: Int32, length: Int) {
+        sendMessage(socket, message: "HTTP/1.1 200 OK\n")
+        sendMessage(socket, message: "Server: Swfit Web Server\n")
+        sendMessage(socket, message: "Content-Length: \(length)\n")
+        sendMessage(socket, message: "Content-Type: text-plain\n")
+        sendMessage(socket, message: "\r\n")
+    }
 }
