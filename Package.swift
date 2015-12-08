159340d083d68c6d991e0a8c7604e864570ca584
tmn <mail@trimn.net>
2015-12-08 19:15:58 +0100

    Add libc to Target list

--- Package.swift
+++ Package.swift
@@ -4,10 +4,13 @@ let package = Package(
     name: "SwiftServer",
     targets: [
         Target(
-            name: "examples",
-            dependencies: [.Target(name: "HTTP")]),
-        Target(
             name: "HTTP",
-            dependencies: [.Target(name: "utils")])
+            dependencies: [.Target(name: "utils"), .Target(name: "libc")]),
+        Target(
+            name: "utils",
+            dependencies: [.Target(name: "libc")]),
+        Target(
+            name: "examples",
+            dependencies: [.Target(name: "HTTP")])
     ]
 )
