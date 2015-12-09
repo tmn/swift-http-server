550649b630a93947c2e8d4238d39973d934f78c3
tmn <mail@trimn.net>
2015-12-09 21:47:55 +0100

    Update example

--- Sources/examples/main.swift
+++ Sources/examples/main.swift
@@ -3,8 +3,16 @@ import libc
 
 var server = Server()
 
-server.addRoute("/test") {
-    print("This is route!")
+server.get("/") { req, res in
+    res.body = "Tri er best!"
+    
+    return .Send(res)
+}
+
+server.get("/test") { req, res in
+    res.body = "Dette er /test"
+    
+    return .Send(res)
 }
 
 server.start()
